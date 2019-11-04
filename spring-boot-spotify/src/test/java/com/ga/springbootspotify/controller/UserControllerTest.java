170723aa185662040d2e2caef30d02a1f4f0ed14
BenjaminKarasik28 <karasik.benjamin28@gmail.com>
2019-11-03 21:31:44 -0500

    controller testing for user completed

--- spring-boot-spotify/src/test/java/com/ga/springbootspotify/controller/UserControllerTest.java
+++ spring-boot-spotify/src/test/java/com/ga/springbootspotify/controller/UserControllerTest.java
@@ -1,25 +1,31 @@
 package com.ga.springbootspotify.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ga.springbootspotify.config.JwtUtil;
+import com.ga.springbootspotify.model.User;
 import com.ga.springbootspotify.service.UserService;
-
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.junit.runner.RunWith;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(UserController.class)
@@ -35,6 +41,11 @@ public class UserControllerTest {
     @MockBean
     private JwtUtil jwtUtil;
 
+    @Mock
+    private List<User> userList;
+
+    @InjectMocks
+    private User user;
 
     @Test
     public void login_Returns200_Success() throws Exception {
@@ -62,7 +73,7 @@ public class UserControllerTest {
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(createUserInJson("batman","robin", "ROLE_ADMIN"));
 
-        when(userService.login(any())).thenReturn("123456");
+        when(userService.createUser(any())).thenReturn("123456");
         mockMvc.perform(requestBuilder)
                 .andExpect(status().isOk());
 
@@ -73,6 +84,53 @@ public class UserControllerTest {
 
         System.out.println(result.getResponse().getContentAsString());
 
+    }
+
+    @Test
+    @WithMockUser(username = "ben", password = "test")
+    public void listUsers_UserController_Success() throws Exception {
+        userList = new ArrayList<>();
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/user/list")
+                .accept(MediaType.APPLICATION_JSON);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().isOk())
+                .andReturn();
+    }
+
+    @Test
+    @WithMockUser(username = "ben", password = "test")
+    public void addSong_Song_Success() throws Exception {
+
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .put("/user/"+ 1L +"/"+ 1L)
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("Authorization", "23421");
+
+        when(userService.addSong(any(), any())).thenReturn(user);
+
+        ObjectMapper mapper = new ObjectMapper();
+        String userMapper = mapper.writeValueAsString(user);
+        System.out.println(userMapper);
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().isOk())
+                .andExpect(content().string(userMapper))
+                .andReturn();
+    }
+
+    @Test
+    @WithMockUser(username = "ben", password = "test", roles = {"ROLE_ADMIN"})
+    public void deleteUserById_UserController_Success() throws Exception {
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .delete("/user/" + 1L)
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("Authorization", "76362");
+
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().isOk());
     }
     private static String createUserInJson (String name, String password) {
         return "{ \"name\": \"" + name + "\", " +
